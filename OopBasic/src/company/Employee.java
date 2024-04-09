1116dcc922e1d56f48f56e5441a9086cf95555af
momoka.sugiura <momoka.sugiura.d088@dirbato.co.jp>
2024-04-09 14:10:18 +0900

    Gitの練習です

--- OopBasic/src/company/Employee.java
+++ OopBasic/src/company/Employee.java
@@ -2,13 +2,13 @@ package company;
 
 public class Employee {
 	//フィールド
-	String name;
-	String department;
-	String position;
-	int employeeId;
+	protected final String name;
+	private final Department department;
+	private final String position;
+	private final int employeeId;
 	
 	//コンストラクター
-	public Employee(String name, String department, String position, int employeeId) {
+	public Employee(String name, Department department, String position, int employeeId) {
 		this.name = name;
 		this.department = department;
 		this.position = position;
@@ -23,6 +23,12 @@ public class Employee {
 	//報告メソッド（オーバーロード）
 	public void report() {
 		report(1);
+	}
+	
+	//会議に参加するメソッド
+	public void joinMeeting() {
+		department.meeting();
+		System.out.println("→上記の会議に参加します。部署:" + department.getName()+"名前:"+name);
 		
 	}
 
--- OopBasic/src/company/ObjectSample.java
+++ OopBasic/src/company/ObjectSample.java
@@ -4,20 +4,24 @@ public class ObjectSample {
 
 	public static void main(String[] args) {
 		//インスタンスの作成
-		var employee = new Employee("鈴木", "営業部", "課長、", 100);
+		var department = new Department("営業部","xx",1000000);
+		var employee = new Employee("鈴木", department, "課長、", 100);
 				
 		//インスタンスメソッドの呼び出し
 		employee.report();
 		employee.report(2);
+		employee.joinMeeting();
 		
 		//1行空ける
 		System.out.println("");
 		
 		//インスタンスの作成
-		var engineer = new Employee("田中","開発部","一般社員",88);
+		var devDepartment = new Department("開発部","yy",0);
+		var engineer = new Employee("田中",devDepartment,"一般社員",88);
 		
 		//インスタンスメソッドの呼び出し
 		engineer.report();
+		engineer.joinMeeting();
 				
 
 	}
