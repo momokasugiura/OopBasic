900d11fc62b7f7b7780886f33ff3f3c2f62f25a2
momoka.sugiura <momoka.sugiura.d088@dirbato.co.jp>
2024-04-10 14:14:48 +0900

    Gitの練習です

--- OopBasic/src/company/Employee.java
+++ OopBasic/src/company/Employee.java
@@ -1,9 +1,10 @@
 package company;
 
-public class Employee {
+//抽象クラスに変更
+public abstract class Employee implements Workable {
 	//フィールド
 	protected final String name;
-	private final Department department;
+	protected final Department department;
 	private final String position;
 	private final int employeeId;
 	
@@ -25,10 +26,14 @@ public class Employee {
 		report(1);
 	}
 	
+	//抽象メソッドに変更
 	//会議に参加するメソッド
-	public void joinMeeting() {
-		department.meeting();
-		System.out.println("→上記の会議に参加します。部署:" + department.getName()+"名前:"+name);
+	public abstract void joinMeeting();
+	
+	//働くメソッドを実装
+	@Override
+	public void work() {
+		System.out.println("正社員として働きます。名前：" + name + slogan);
 		
 	}
 
